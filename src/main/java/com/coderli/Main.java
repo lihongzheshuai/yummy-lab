eab6d58919389e721e9574cf80bbe43ec0068555
lihongzhe <wushikezuo@gmail.com>
2024-07-03 22:28:19 +0800

    delete spring mvc code

--- src/main/java/com/coderli/Main.java
+++ src/main/java/com/coderli/Main.java
@@ -3,5 +3,6 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Map<String, String> map1 = Map.of("a", "1", "b", "2");
+        System.out.println("Hello World");
     }
 }
